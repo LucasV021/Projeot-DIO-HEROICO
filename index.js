f6b0e0ddddf437e404f4a546ca62a3fdfeaf8408
lucas <lucas.dev021@gmail.com>
2023-11-01 18:21:10 -0300

    Desafio2

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 
 // Desafio de classificação
 var nomeDoHeroi = "Lucas Vieira Ferrari"
-var xpDoHeroi = 10.000
+var xpDoHeroi = 2000.000
 var definicao = ""
 if(xpDoHeroi === 1000){
     definicao = "Ferro"
 }
-else if (xpDoHeroi >1.001 && xpDoHeroi <2.000){
+else if (xpDoHeroi >1.001 && xpDoHeroi <=2.000){
     definicao = "Bronze"
 }
-else if (xpDoHeroi >2.001 && xpDoHeroi <2.001){
+else if (xpDoHeroi >2.001 && xpDoHeroi <=2.001){
     definicao = "Prata"
 }
-else if(xpDoHeroi >6.001 && xpDoHeroi <7.000){
+else if(xpDoHeroi >6.000 && xpDoHeroi <=7.000){
     definicao = "Ouro"
 }
-else if (xpDoHeroi > 7.001 && xpDoHeroi <8.000){
+else if (xpDoHeroi > 7.001 && xpDoHeroi <=8.000){
     definicao = "Platina"
 }
-else if (xpDoHeroi >8.001  && xpDoHeroi <9.000){
+else if (xpDoHeroi >8.001  && xpDoHeroi <=9.000){
     definicao = "Ascendente"
 }
-else if (xpDoHeroi >9.001  && xpDoHeroi <10.000){
+else if (xpDoHeroi >9.001  && xpDoHeroi <=10.000){
     definicao = "Imortal"
 }
 else {
-    (definicao= "Radiante")
+    definicao = "Radiante"
 }
-console.log(`O Herói de nome ${nomeDoHeroi} está no nível de ${definicao }. `)
+console.log(`O Herói de nome ${nomeDoHeroi} está no nível de ${definicao } com uma quantidade incrivel de XP de ${xpDoHeroi} `)
