9183c0b58a8aea90b6fbbcf4da6122f1d6a9dadc
lucas <lucas.dev021@gmail.com>
2023-11-03 19:09:37 -0300

    integrando FOR COM IF/ELSE IF/ ELSE

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // historia do heroi inicial 
 var xpInicial = 1
 console.log(`O jovem heroí q saiu de sua cidade com um xp inicial de ${xpInicial} achou uma caverna magica e nessa caverna encontrou um elixir supremo de XP, tomou
-esse elixir e se sentiu estranho e desmaiou `)
+esse elixir e se sentiu estranho e desmaiou. `)
 for (var i = 1 ; i <= 200000 ; i += 100000 ){
     xpInicial += 100000 
 }
@@ -32,4 +32,5 @@ else if (xpInicial >9.001  && xpInicial <=10.000){
 else {
     definicao = "Radiante"
 }
-console.log(`O Herói de nome ${nomeDoHeroi} acordou abriu sua hotbar e conferiu que estava na classificação de ${definicao } com uma quantidade incrivel de XP parando no nivel ${xpInicial}.`)
+console.log(`O Herói de nome ${nomeDoHeroi} acordou, abriu sua hotbar e conferiu que estava na classificação de ${definicao}, percebendo assim que tinha recebido uma incrível quantidade de XP, parando assim no nível ${xpInicial}.`);
+
